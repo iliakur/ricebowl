ad666e9ebc3f688be896099a443de3683d1a419b
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2019-04-25 20:51:19 +0200

    10 seconds timeout on battery warning

--- battery_status_check.sh
+++ battery_status_check.sh
@@ -5,4 +5,4 @@ set -e
 # - parse with awk, treating commas as separators
 # - if the status contains "Discharging" look at the second field, which is the percentage left
 # - if that's lower than the threshold (10 percent), send a notification
-acpi -b | awk -F , '/Discharging/ {if (int($2) < 10) system("notify-send -u critical \"Plug me in please!\" -t 1000")}'
+acpi -b | awk -F , '/Discharging/ {if (int($2) < 10) system("notify-send -u critical \"Plug me in please!\" -t 10000")}'
