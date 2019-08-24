7e198bd09058654c1be5b1e459e482b032739291
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2019-08-24 17:56:08 +0200

    Keyboard remaps in separate script, can be run without logout

---
+++ scripts/ikkbremaps.sh
@@ -0,0 +1,9 @@
+set -e
+
+# Use both shifts to cycle through layouts.
+# Swap left control, alt, super for better emacs experience.
+# Capslock key is remapped to Esc for better vim experience.
+setxkbmap \
+    -layout us,ru,us \
+    -variant colemak,, \
+    -option grp:shifts_toggle,ctrl:swap_lalt_lctl_lwin,caps:escape
