e7aa75823aac10960ab93b0c70ffe789e30b7444
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2020-03-21 20:28:00 +0100

    capslock is available via ESC key

--- scripts/ikkbremaps.sh
+++ scripts/ikkbremaps.sh
@@ -2,8 +2,8 @@ set -e
 
 # Use both shifts to cycle through layouts.
 # Swap left control, alt, super for better emacs experience.
-# Capslock key is remapped to Esc for better vim experience.
+# Capslock key is swapped with Esc for better vim experience.
 setxkbmap \
     -layout us,ru,us \
     -variant colemak,, \
-    -option grp:shifts_toggle,ctrl:swap_lalt_lctl_lwin,caps:escape
+    -option grp:shifts_toggle,ctrl:swap_lalt_lctl_lwin,caps:swapescape
