2a718ef18dc95f740f0b4e749d43195068b87bcc
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2022-10-13 14:11:43 +0200

    first version of Datadog setup script

---
+++ dd_setup.sh
@@ -0,0 +1,8 @@
+brew install --cask iterm2
+brew install pyright
+brew install ripgrep
+brew install --cask dropbox
+brew install --cask docker
+brew install --cask karabiner-elements
+mkdir -p ~/.config/karabiner
+ln -s --force "${PWD}"/karabiner.json ~/.config/karabiner
